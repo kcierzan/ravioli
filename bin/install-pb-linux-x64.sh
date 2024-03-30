2ebde17ae62a04b2fba1afff4cf21b97dbc3d55f
Kyle Cierzan <kcierzan@gmail.com>
2024-03-30 15:28:04 -0400

    Replace aria2c w/ wget

--- bin/install-pb-linux-x64.sh
+++ bin/install-pb-linux-x64.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-aria2c -x 10 https://github.com/pocketbase/pocketbase/releases/download/v0.22.7/pocketbase_0.22.7_linux_amd64.zip \
+wget https://github.com/pocketbase/pocketbase/releases/download/v0.22.7/pocketbase_0.22.7_linux_amd64.zip \
   && unzip pocketbase_0.22.7_linux_amd64.zip pocketbase \
   && mv pocketbase pb_bin \
   && rm pocketbase_0.22.7_linux_amd64.zip
